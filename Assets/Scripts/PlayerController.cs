fd5d7465fe40388a202262867d9dd11f3a4018be
Chiharu <108509922+haru-kita@users.noreply.github.com>
2023-09-05 21:27:16 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Animator anim;
+    private float speed = 5.0f;
+
+    [SerializeField] PlayerStatusSO playerStatusSO;
+    [SerializeField] Text hpText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        //HPを取得
+        hpText.GetComponent<Text>().text = "HP:" + playerStatusSO.ToString(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //HPを取得
+        hpText.GetComponent<Text>().text = "HP:" + playerStatusSO.ToString();
+
+        // 前移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = transform.forward * speed;
+            this.anim.SetBool("Run" , true);
+        }
+
+        // 後移動
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = - transform.forward * speed;
+            this.anim.SetBool("Run" , true);
+        }
+
+        // 右移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+            this.anim.SetBool("Run" , true);
+        }
+
+        // 左移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * speed;
+            this.anim.SetBool("Run" , true);
+        }
+
+        //キーを離すとIdleに戻る
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)  )
+        {
+            anim.SetBool("Run" , false);
+        }
+
+        //キーマウスがクリックされた場合
+        if (Input.GetMouseButtonDown (0))
+        {
+            //Animatorコンポーネントを取得し"Attock"をtrueにする
+            this.anim.SetTrigger("Attack");
+        }
+
+    }
+
+    //他のオブジェクトと接触した場合の処理
+    void OnCollisionEnter(Collision col)
+    {
+        //HPを減らす
+        playerStatusSO.HP = playerStatusSO.HP - 10;
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerStatusSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class PlayerStatusSO : ScriptableObject
+{
+    [SerializeField] int hp;
+    [SerializeField] int mp;
+    [SerializeField] int attack;
+    [SerializeField] int defence;
+
+
+    //‚±‚ê‚ð‘‚©‚È‚¢‚ÆƒGƒ‰[‚É‚È‚é
+    public int HP { get => hp; set => hp = value; }
+}
