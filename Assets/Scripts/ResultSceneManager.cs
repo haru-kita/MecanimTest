c9d55d58f9c5ead56ce52452ee1a84611ab763f1
Chiharu <108509922+haru-kita@users.noreply.github.com>
2023-09-22 23:07:16 +0900

    得点表示の不具合修正
    ・リザルドシーンのスクリプト2つを1つに纏めた。

--- Assets/Scripts/ResultSceneManager.cs
+++ Assets/Scripts/ResultSceneManager.cs
@@ -7,6 +7,7 @@ public class ResultSceneManager : MonoBehaviour
     public Button titleButton;
     public Button startButton;
     public Button exitButton;
+    public Text scoreText;
 
     private void Start()
     {
@@ -16,27 +17,35 @@ public class ResultSceneManager : MonoBehaviour
         startButton.onClick.AddListener(StartGame);
         // Exitボタンがクリックされたときの処理を設定
         exitButton.onClick.AddListener(ExitGame);
+
+        // スコアを取得してテキストに反映
+        int score = PlayerPrefs.GetInt("LastScore", 0);
+        string formattedScore = score.ToString("D4"); // 4桁のフォーマットに変換してテキストに表示
+        scoreText.text = "Score: " + formattedScore + "pt";
     }
 
-    //Titleボタンがクリックされたときの処理
+    // Titleボタンがクリックされたときの処理
     private void BackGame()
     {
-        //Titleシーンに遷移
+        // Titleシーンに遷移
         SceneManager.LoadScene("TitleScene");
     }
 
-    //STARTボタンがクリックされたときの処理
+    // STARTボタンがクリックされたときの処理
     private void StartGame()
     {
-        //バトルシーンに遷移
+        // スコアを保存
+        int score = PlayerPrefs.GetInt("LastScore", 0);
+        PlayerPrefs.SetInt("LastScore", score);
+
+        // バトルシーンに遷移
         SceneManager.LoadScene("BattleScene");
     }
 
-    //Exitボタンがクリックされたときの処理
+    // Exitボタンがクリックされたときの処理
     private void ExitGame()
     {
-        //アプリを落とす
+        // アプリを落とす
         Application.Quit();
     }
-
 }
